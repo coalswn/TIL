c8ae40e7ebf1e5a4780a23694f1ace35c952bb3a
chaeCHAE <alswndla7189@naver.com>
2025-07-23 22:05:23 +0900

    nico js 복습 강의 수강

--- Vanilla_JS/bak/1-2.js
+++ Vanilla_JS/bak/1-2.js
@@ -11,7 +11,7 @@ console.log("your new name is " + myName);  // 이전 값에 영향 안감
 0 // off = false = 파이썬 False
 1 // on = true = 파이썬 True
 const amIFat = null; // null : 값이 없음을 알려주기 위해 사용 = 파이썬 None
-let something; // undefined : 메모리는 있는데 값이 주어졌는데 그 값이 없는 것.
+let something; // undefined : 메모리는 있는데 그 값이 없는 것.
 console.log(something, amIFat); 
 
 
@@ -20,6 +20,7 @@ const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
 
 // Get Item from Array
 console.log(daysOfWeek[4]); // fri
+daysOfWeek[4] = "today" // 업데이트 가능
 console.log(daysOfWeek); // 6
 
 // Add one more day to the array
@@ -42,21 +43,22 @@ player.points = player.points + 15;
 /* 4. 함수 */ 
 function plus(firstNumber, secondNumber) {
     console.log(firstNumber + secondNumber); // plus();일 때 NaN(= Not a number. 숫자가 아님)
-}
-// console.log(firstNumber); // firstNumber 변수는 블록 밖에서 존재할수없으므로 에러남.
+} // <- 이렇게 실행할 블록을 만들어서...
+// console.log(firstNumber); // firstNumber 변수는 블록 밖에서 존재할 수 없으므로 에러남.
+
 function divide(a, b) {
     console.log(a / b);
 }
 
-plus(8, 60); // 데이터를 넣고 실행해서 function으로 데이터 전달!
+plus(8, 60); // 데이터를 넣고 실행해서 function으로 데이터 전달!  <- 이렇게 실행!
 divide(98, 20); // 첫번째 변수, 두번째 변수에 각각 '순서'대로 argument 데이터 전달.
 
 const player = {
-    name:"nico",
-    sayHello: function(otherPersonsName) {
+    name : "nico",
+    sayHello : function(otherPersonsName) {
         console.log("hello! " + otherPersonsName + " nice to meet you!");
     },
-};
+}; // 객체와 함수 혼종
 
 player.sayHello("lynn");
 player.sayHello("nico");
