b7927d73b7e6bdf597d9824cec21408325e54cbf
chaeCHAE <alswndla7189@naver.com>
2025-07-28 21:03:06 +0900

    nico js 객체, 함수 복습 강의 수강

--- Vanilla_JS/app.js
+++ Vanilla_JS/app.js
@@ -1,12 +1,20 @@
-function plus(firstNumber, secondNumber) {
+function plus01() {
+    console.log(2 + 2); 
+} 
+plus01(); // 꼭 실행 필요!
+
+alert("lalalalala"); // alert 이란 함수 안에 값을 넣는 것!
+
+function plus(firstNumber, secondNumber) { 
+    // 다용도로 같은 코드를 활용하기 위해 밖에서 데이터를 넣을 수 있게 하는 게 좋음
     console.log(firstNumber + secondNumber); // plus();일 때 NaN(= Not a number. 숫자가 아님)
-}
-// console.log(firstNumber); // firstNumber 변수는 블록 밖에서 존재할수없으므로 에러남.
+} // <- 이렇게 실행할 블록을 만들어서...
+// console.log(firstNumber); // firstNumber 변수는 블록 밖에서 존재 할 수 없으므로 에러남.
 function divide(a, b) {
     console.log(a / b);
 }
 
-plus(8, 60); // 데이터를 넣고 실행해서 function으로 데이터 전달!
+plus(8, 60); // 데이터를 넣고 실행해서 function으로 데이터 전달!  <- 이렇게 실행!
 divide(98, 20); // 첫번째 변수, 두번째 변수에 각각 '순서'대로 argument 데이터 전달.
 
 const player = {
@@ -14,7 +22,13 @@ const player = {
     sayHello: function(otherPersonsName) {
         console.log("hello! " + otherPersonsName + " nice to meet you!");
     },
-};
+}; // 객체와 함수 혼종
 
-player.sayHello("lynn");
+player.sayHello("lynn"); // console.log(); 와 같은 구조
 player.sayHello("nico");
+
+function minusFive(num) {
+    console.log(num - 5);
+}
+minusFive(5, 10, 15, 12, 13, 18); // 많은 데이터를 넣어도 5만 적용되어 0이 나옴.
+
--- Vanilla_JS/bak/1-2.js
+++ Vanilla_JS/bak/1-2.js
@@ -39,13 +39,22 @@ player.fat = false; // const로 선언되었어도 내부요소는 업데이트 
 player.lastName = "potato"; // 프로퍼티 추가 가능
 player.points = player.points + 15;
 
+console.log(player, console); // console 도 function으로 이루어진 객체!
+
 
 /* 4. 함수 */ 
-function plus(firstNumber, secondNumber) {
+function plus01() {
+    console.log(2 + 2); 
+} 
+plus01(); // 꼭 실행 필요!
+
+alert("lalalalala"); // alert 이란 함수 안에 값을 넣는 것!
+
+function plus(firstNumber, secondNumber) { 
+    // 다용도로 같은 코드를 활용하기 위해 밖에서 데이터를 넣을 수 있게 하는 게 좋음
     console.log(firstNumber + secondNumber); // plus();일 때 NaN(= Not a number. 숫자가 아님)
 } // <- 이렇게 실행할 블록을 만들어서...
-// console.log(firstNumber); // firstNumber 변수는 블록 밖에서 존재할 수 없으므로 에러남.
-
+// console.log(firstNumber); // firstNumber 변수는 블록 밖에서 존재 할 수 없으므로 에러남.
 function divide(a, b) {
     console.log(a / b);
 }
@@ -54,11 +63,16 @@ plus(8, 60); // 데이터를 넣고 실행해서 function으로 데이터 전달
 divide(98, 20); // 첫번째 변수, 두번째 변수에 각각 '순서'대로 argument 데이터 전달.
 
 const player = {
-    name : "nico",
-    sayHello : function(otherPersonsName) {
+    name:"nico",
+    sayHello: function(otherPersonsName) {
         console.log("hello! " + otherPersonsName + " nice to meet you!");
     },
 }; // 객체와 함수 혼종
 
-player.sayHello("lynn");
+player.sayHello("lynn"); // console.log(); 와 같은 구조
 player.sayHello("nico");
+
+function minusFive(num) {
+    console.log(num - 5);
+}
+minusFive(5, 10, 15, 12, 13, 18); // 많은 데이터를 넣어도 5만 적용되어 0이 나옴.
