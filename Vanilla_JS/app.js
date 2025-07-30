28033a0c02e70652b0a5838e3b68078be6ed50b0
chaeCHAE <alswndla7189@naver.com>
2025-07-30 22:13:50 +0900

    nico js Returns 강의 수강

--- Vanilla_JS/app.js
+++ Vanilla_JS/app.js
@@ -1,34 +1,35 @@
-function plus01() {
-    console.log(2 + 2); 
-} 
-plus01(); // 꼭 실행 필요!
-
-alert("lalalalala"); // alert 이란 함수 안에 값을 넣는 것!
-
-function plus(firstNumber, secondNumber) { 
-    // 다용도로 같은 코드를 활용하기 위해 밖에서 데이터를 넣을 수 있게 하는 게 좋음
-    console.log(firstNumber + secondNumber); // plus();일 때 NaN(= Not a number. 숫자가 아님)
-} // <- 이렇게 실행할 블록을 만들어서...
-// console.log(firstNumber); // firstNumber 변수는 블록 밖에서 존재 할 수 없으므로 에러남.
-function divide(a, b) {
-    console.log(a / b);
+const age = 96;
+// function 은 일을 수행하고 그 결과를 return해서 알려주는 것
+function calculateKrAge(ageOfForeigner) { 
+   ageOfForeigner + 2; 
+   return "hello"; // return은 함수가 함수 밖과 소통하는 방법. 함수 안에서 return을 하면,  
 }
+const krAge = calculateKrAge(age); // 함수를 실행시 calculateKrAge의 반환값이 됨
+console.log(krAge); // return안하면 undefined
+// calculateKrAge의 반환값인 hello(string)가 반환됨
 
-plus(8, 60); // 데이터를 넣고 실행해서 function으로 데이터 전달!  <- 이렇게 실행!
-divide(98, 20); // 첫번째 변수, 두번째 변수에 각각 '순서'대로 argument 데이터 전달.
-
-const player = {
-    name:"nico",
-    sayHello: function(otherPersonsName) {
-        console.log("hello! " + otherPersonsName + " nice to meet you!");
+const calculator = {
+    plus : function (a, b) {
+        return a + b;
     },
-}; // 객체와 함수 혼종
+    minus: function (a, b) {
+        return a - b;
+    },
+    times: function (a, b) {
+        return a * b;
+    },
+    divide: function (a, b) {
+        return a / b;
+    },
+    power: function (a, b) {
+        return a ** b;
+    },
+};
+const plusResult = calculator.plus(2, 3); // 변수 자체가 반환값. 함수 외부에서 값을 얻고 활용 가능 
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);
 
-player.sayHello("lynn"); // console.log(); 와 같은 구조
-player.sayHello("nico");
-
-function minusFive(num) {
-    console.log(num - 5);
-}
-minusFive(5, 10, 15, 12, 13, 18); // 많은 데이터를 넣어도 5만 적용되어 0이 나옴.
+console.log(plusResult); // 오오...console에 변수만 쳐도 값 나옴
 
--- Vanilla_JS/bak/1-2.js
+++ Vanilla_JS/bak/1-2.js
@@ -76,3 +76,40 @@ function minusFive(num) {
     console.log(num - 5);
 }
 minusFive(5, 10, 15, 12, 13, 18); // 많은 데이터를 넣어도 5만 적용되어 0이 나옴.
+
+
+/* 5. Returns */ 
+const age = 96;
+// function 은 일을 수행하고 그 결과를 return해서 알려주는 것
+function calculateKrAge(ageOfForeigner) { 
+   ageOfForeigner + 2; 
+   return "hello"; // return은 함수가 함수 밖과 소통하는 방법. 함수 안에서 return을 하면,  
+}
+const krAge = calculateKrAge(age); // 함수를 실행시 calculateKrAge의 반환값이 됨
+console.log(krAge); // return안하면 undefined
+// calculateKrAge의 반환값인 hello(string)가 반환됨
+
+const calculator = {
+    plus : function (a, b) {
+        return a + b;
+    },
+    minus: function (a, b) {
+        return a - b;
+    },
+    times: function (a, b) {
+        return a * b;
+    },
+    divide: function (a, b) {
+        return a / b;
+    },
+    power: function (a, b) {
+        return a ** b;
+    },
+};
+const plusResult = calculator.plus(2, 3); // 변수 자체가 반환값. 함수 외부에서 값을 얻고 활용 가능 
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);
+
+console.log(plusResult); // 오오...console에 변수만 쳐도 값 나옴
